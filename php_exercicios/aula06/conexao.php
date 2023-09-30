b73a79c80723a6aedb2b7a241ed2189cf00328a3
Sérgio de Souza Lima Sobrinho <sobrinhosergio00@gmail.com>
2023-09-30 20:12:26 -0300

    Update: Atualizando proj

--- php_exercicios/aula06/conexao.php
+++ php_exercicios/aula06/conexao.php
@@ -1,6 +1,12 @@
 <?php
   function conectar(){
-    return new PDO('mysql:dbname=bd_produtos;host=localhost;charset=utf8',
-    'root', '', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    return new PDO('
+                    mysql:dbname=bd_produtos;
+                    host=localhost;
+                    charset=utf8',
+                    'root',
+                    '', 
+                    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+                  );
   }
 ?>
