bc45fcf0ca2cf4c4cb587efb489bd14a2ce8c433
Sérgio de Souza Lima Sobrinho <sobrinhosergio00@gmail.com>
2023-11-25 11:39:44 -0300

    Atualizando proj

--- web/aula11-2/aula10/api/src/conexao.php
+++ web/aula11-2/aula10/api/src/conexao.php
@@ -2,10 +2,12 @@
 
 function conectar() {
     return new PDO(
-        'mysql:dbname=aula10;host=localhost;charset=utf8',
-        'root',
-        '',
-        [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION ]
+        'mysql:dbname=aula10;
+         host=localhost;
+         charset=utf8',
+         'root',
+         '',
+         [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION ]
     );
 }
 ?>
