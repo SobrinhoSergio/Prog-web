210394a12f9793a657ccf8e25e8871c5ca8db379
Sérgio de Souza Lima Sobrinho <sobrinhosergio00@gmail.com>
2024-03-31 12:30:36 -0300

    Adicionando proj

--- código-aula/aula06/cadastrar.php
+++ código-aula/aula06/cadastrar.php
@@ -6,24 +6,38 @@ $descricao = readline("Descrição: ");
 $estoque = readline("Estoque: ");
 $preco = readline("Preço (R$): ");
 
+// Validar a descrição
+if (strlen($descricao) < 2 || strlen($descricao) > 100) {
+    die("A descrição deve ter entre 2 e 100 caracteres.");
+}
+
+// Validar o estoque
+if (!is_numeric($estoque) || $estoque < 0) {
+    die("Estoque deve ser um número positivo.");
+}
+
+// Validar o preço
+if (!is_numeric($preco) || $preco <= 0) {
+    die("Preço deve ser um número positivo maior que zero.");
+}
+
 $pdo = null;
 
 try {
     
     $pdo = conectar();
     
-    $ps = $pdo->prepare( 'INSERT INTO produto ( descricao, estoque, preco_compra )
-                           VALUES ( :descricao, :estoque, :preco )' );
+    $ps = $pdo->prepare( 'INSERT INTO produto (descricao, estoque, preco_compra)
+                           VALUES (:descricao, :estoque, :preco)' );
    
-    $ps->execute( ['descricao' => $descricao, 'estoque' => $estoque, 'preco' => $preco ] );
+    $ps->execute(['descricao' => $descricao, 'estoque' => $estoque, 'preco' => $preco]);
 
     echo 'Inserido com sucesso.';
-
     
-    
-}catch(PDOException $e){
-    die("Error ao consultar o banco de dados: " . $e->getMessage());
+} catch(PDOException $e) {
+    die("Erro ao consultar o banco de dados: " . $e->getMessage());
 }
+
 
 
 /*$descricao = readline("Descrição: ");
